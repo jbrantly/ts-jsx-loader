ed62c4e1e32e41e98201a556dfb8e2d986589b36
James Brantly <james@jbrantly.com>
2015-03-19 19:27:36 -0400

    Update definitions to work with React v0.13 definitions

--- react-jsx.d.ts
+++ react-jsx.d.ts
@@ -1,7 +1,11 @@
-declare module React {
-    interface TopLevelAPI {
-        jsx: (jsx?: string) => React.ReactElement<any>;
-        
-        __spread: any; // for JSX Spread Attributes
-    }
+// This file is intended to be used with the DefinitelyTyped React v0.13 definitions
+
+declare module "react" {
+		function jsx(jsx?: string): ReactElement<any>;
+		function __spread(...args: any[]): any; // for JSX Spread Attributes
+}
+
+declare module "react/addons" {
+		function jsx(jsx?: string): ReactElement<any>;
+		function __spread(...args: any[]): any; // for JSX Spread Attributes
 }
