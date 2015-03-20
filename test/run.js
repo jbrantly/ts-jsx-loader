127f45a580d0dddee1ad51b19db5a3377b8184b8
James Brantly <james@jbrantly.com>
2015-03-19 20:48:36 -0400

    Refactor test runner to support passing query to loader

--- test/run.js
+++ test/run.js
@@ -3,25 +3,36 @@ var fs = require('fs');
 var path = require('path');
 var jsxLoader = require('../index');
 
-var context = {
+function runTests(tests, query) {
+  
+  var context = {
     cacheable: function() {},
-    emitError: function() {}
-}
-
-var files = fs.readdirSync(__dirname)
-    .filter(function(f) { return path.extname(f) == '.test' })
-    .forEach(function(f) {
-        describe(path.basename(f, '.test'), function() {
-            it('should have the correct output', function(done){
-                fs.readFile(path.join(__dirname, f), {encoding: 'utf-8'}, function(err, testFile) {
-                    if (err) return done(err)
-                    fs.readFile(path.join(__dirname, f+'.output'), {encoding: 'utf-8'}, function(err, outputFile) {
-                        if (err) return done(err)
-                        
-                        assert.equal(jsxLoader.call(context, testFile), outputFile);
-                        done();
-                    })
-                })
+    emitError: function() {},
+    query: query
+  }
+  
+  tests.forEach(function(f) {
+    var filename = f + '.test';
+    describe(filename, function() {
+      it('should have the correct output', function(done){
+        fs.readFile(path.join(__dirname, filename), {encoding: 'utf-8'}, function(err, testFile) {
+          if (err) return done(err)
+          fs.readFile(path.join(__dirname, filename+'.output'), {encoding: 'utf-8'}, function(err, outputFile) {
+            if (err) return done(err)
+            
+            assert.equal(jsxLoader.call(context, testFile), outputFile);
+            done();
           })
         })
+      })
     })
+  })
+}
+
+runTests([
+  'comment',
+  'error',
+  'harmony',
+  'jsx-comment',
+  'stringtemplate'
+])
